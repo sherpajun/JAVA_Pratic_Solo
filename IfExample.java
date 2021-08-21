e41adcec876a30da0a9170f7ab1f831500b280c2
sherpajun <jun1781@naver.com>
2021-08-21 18:48:56 +0900

    if test
    pratice wroking 'if'

---
+++ IfExample.java
@@ -0,0 +1,10 @@
+public class IfExample {
+    public static void main (String[]args){
+        int i=14;
+        if (i>=14) {
+            System.out.println("MiddleSchoolStudent");
+        }else{
+            System.out.println("ElementrySchoolStudent");
+        }
+    }
+}
---
+++ IfExample2.java
@@ -0,0 +1,22 @@
+public class IfExample2 {
+    public static void main(String[] args) {
+        int age = 9;
+        int charge;
+        if (age < 8) {
+            charge = 1000;
+            System.out.println("미취학 아동");
+        } if (age < 14) {
+            charge = 2000;
+            System.out.println("취학 아동");
+
+        } if (age < 20) {
+            charge = 2000;
+            System.out.println("학생입니다");
+
+        } else {
+            charge = 3000;
+            System.out.println("성인입니다");
+        }
+        System.out.println("입장료는"+charge+"원입니다");
+    }
+}
