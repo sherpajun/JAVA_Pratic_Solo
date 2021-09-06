f576d37f54beec2736062ea54c961ec5a76ba08c
sherpajun <87353476+sherpajun@users.noreply.github.com>
2021-09-07 00:06:18 +0900

    Add files via upload

---
+++ ArrayTest.java
@@ -0,0 +1,10 @@
+package DoitJavaPratice.array;
+
+public class ArrayTest {
+    public static void main (String []args){
+        int[] num = new int[]{1,3,5,7,9,12,15,18,21,23};
+        for(int i =0; i< num.length;i++){
+            System.out.println(num[i]+num[i]);
+        }
+    }
+}
